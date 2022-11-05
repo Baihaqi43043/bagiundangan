865e6b0d88f1527f31e33fd1f66d39fc61879bc6
baihaqi43043 <baihaqi43043@gmail.com>
2022-11-05 17:01:54 +0700

    instaal bosstrap

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'dbacc2b0378ecea282f0247df3ecb8d7dee99e41',
+        'reference' => 'edf854a2a0fd4bf6ded6eed50ec7fd384fbfa1f5',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -391,7 +391,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'dbacc2b0378ecea282f0247df3ecb8d7dee99e41',
+            'reference' => 'edf854a2a0fd4bf6ded6eed50ec7fd384fbfa1f5',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -428,6 +428,15 @@
             'install_path' => __DIR__ . '/../laravel/tinker',
             'aliases' => array(),
             'reference' => 'dff39b661e827dae6e092412f976658df82dbac5',
+            'dev_requirement' => false,
+        ),
+        'laravel/ui' => array(
+            'pretty_version' => 'v3.4.6',
+            'version' => '3.4.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/ui',
+            'aliases' => array(),
+            'reference' => '65ec5c03f7fee2c8ecae785795b829a15be48c2c',
             'dev_requirement' => false,
         ),
         'league/commonmark' => array(
